e391cfa5a8740a5588a7536bcb543766ecb311a4
Guilherme S <guib225@gmail.com>
2022-03-18 12:25:19 -0300

    Adiciona verificações e returns

--- src/api/views.js
+++ src/api/views.js
@@ -4,27 +4,37 @@ const bot = require('../bot');
 async function send(req, res) {
     const { to, content } = req.query;
 
-    if (!to.startsWith('55') || to.length !== 13) {
-        res.send({
-            error: false,
+    if (!to || !content) {
+        return res.status(400).json({
+            error: true,
             msg: 'Parâmetros inválidos.',
-        }, 422);
+        });
     }
 
-    try {
-        await bot.sendText(`${to}@c.us`, content);
-        res.send({
+    if (!to.startsWith('55') || to.length !== 13) {
+        return res.status(422).json({
+            error: true,
+            msg: 'Parâmetros inválidos.',
+        });
+    }
+
+    bot.sendText(`${to}@c.us`, content).then((result) => {
+        res.json({
             error: false,
             msg: 'Mensagem enviada com sucesso.',
+            result,
         });
-    } catch (error) {
-        console.error(error);
+    }).catch((result) => {
+        console.error(result);
 
-        res.send({
+        res.json({
             error: true,
             msg: 'Não foi possível enviar a mensagem.',
+            result,
         });
-    }
+    });
+
+    return null;
 }
 
 module.exports = {
