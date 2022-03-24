679ee173d8640df3c04df0f6c90e6b84628e2b0c
Guilherme S <guibs225@gmail.com>
2022-03-24 12:00:38 -0300

    Adiciona user-agent

--- src/bot.js
+++ src/bot.js
@@ -6,6 +6,7 @@ let bot;
     bot = await venom.create({
         session: 'bot',
         multidevice: true,
+        browserArgs: ['--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36'],
     });
 })();
 
