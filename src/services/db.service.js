cb18655bcfe4f0b49d21eea20d9b0870315029d4
Guilherme S <guibs225@gmail.com>
2022-04-03 03:40:03 -0300

    fix: corrige instância inválida

--- src/services/db.service.js
+++ src/services/db.service.js
@@ -1,6 +1,9 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('database.db');
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: 'database.db',
+});
 
 (async () => {
     await sequelize.sync();
