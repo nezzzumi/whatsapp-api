790ee99dd51cee02acc6671da0ef686b968f8fe3
Guilherme S <guibs225@gmail.com>
2022-04-03 03:56:19 -0300

    refactor: remove sync e desativa `logging`

--- src/services/db.service.js
+++ src/services/db.service.js
@@ -3,12 +3,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize({
     dialect: 'sqlite',
     storage: 'database.db',
+    logging: false,
 });
 
-(async () => {
-    await sequelize.sync();
-})();
-
-module.exports = {
-    sequelize,
-};
+module.exports = sequelize;
