72d3ef863cf8af6b2a4f1b4a48fbb32d112a4b04
Guilherme S <guibs225@gmail.com>
2022-04-03 23:34:11 -0300

    refactor: remove função anônima e adiciona callbacks ao Promise

--- src/services/bot.service.js
+++ src/services/bot.service.js
@@ -1,14 +1,15 @@
+const console = require('console');
 const venom = require('venom-bot');
 
 let bot;
 
-(async () => {
-    bot = await venom.create({
-        session: 'bot',
-        multidevice: true,
-        browserArgs: ['--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36'],
-    });
-})();
+venom.create({
+    session: 'bot',
+    multidevice: true,
+    browserArgs: ['--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36'],
+}).then((result) => {
+    bot = result;
+}).catch(console.error);
 
 async function sendText(to, content) {
     return bot.sendText(to, content);
