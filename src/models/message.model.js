6f8887022c8e85877261468519ed58c0a76b5a27
Guilherme S <guibs225@gmail.com>
2022-05-06 21:41:14 -0300

    refactor: remove require desnecessário

--- src/models/message.model.js
+++ src/models/message.model.js
@@ -1,6 +1,5 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../services/db.service');
-const User = require('./user.model');
 
 const Message = sequelize.define('Message', {
     id: {
