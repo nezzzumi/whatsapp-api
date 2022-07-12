ca4cb08f0fee6ae741a1e31e413b52b92831ff5b
Guilherme <suporte.ti1@redehg.com.br>
2022-07-12 17:52:23 -0300

    fix: caractere inválido

--- src/controllers/bot.controller.js
+++ src/controllers/bot.controller.js
@@ -24,7 +24,7 @@ async function send(req, res) {
       msg: 'Parâmetros inválidos.',
     });
   }
-!
+
   if (!to.startsWith('55') || ![12, 13].includes(to.length)) {
     return res.status(422).json({
       error: true,
