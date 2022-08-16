af803273f9973a35672d76efd5b8d586c186a749
Guilherme <suporte.ti1@redehg.com.br>
2022-08-16 16:06:45 -0300

    feat: utiliza porta da variável de ambiente `PORT`

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const apiRouter = require('./routes/api.route');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/api', apiRouter);
