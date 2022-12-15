73ae78128323e585da17a4085fa76a461341de03
Guilherme <guilherme0xff@proton.me>
2022-12-15 08:56:11 -0300

    improvement: ativa modo headless

--- src/services/implementations/WhatsAppService.ts
+++ src/services/implementations/WhatsAppService.ts
@@ -9,7 +9,7 @@ import { HttpError } from '../../errors/HttpError';
 export class WhatsAppService implements IService {
   private bot: Client = new Client({
     authStrategy: new LocalAuth(),
-    puppeteer: { headless: false },
+    puppeteer: { headless: true },
     userAgent:
       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36',
   });
